93db8a1d3e89d258c1284162550846e087f39878
Avinash <avinashmarbhal1994@gmail.com>
2026-02-24 00:07:39 +0530

    constants changes

--- src/app/models/constants.ts
+++ src/app/models/constants.ts
@@ -1,6 +1,6 @@
 export class Constants {
-    public static APP_NAME_STR1: string = 'Demo';
-    public static APP_NAME_STR2: string = 'Demo';
+    public static APP_NAME_STR1: string = 'Luxe';
+    public static APP_NAME_STR2: string = 'Stay';
     public static APP_NAME: string = `${Constants.APP_NAME_STR1} ${Constants.APP_NAME_STR2}`;
 
     public static MOCK_DESTINATIONS_ARR: any = [
